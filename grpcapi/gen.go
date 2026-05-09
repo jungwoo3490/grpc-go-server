f976f86b63c3a972c69a8cce76850d17b5071423
jungwoo3490 <jungwoo3490@naver.com>
2026-05-09 23:49:31 +0900

    fix: proto codegen 경로 옵션 수정

--- grpcapi/gen.go
+++ grpcapi/gen.go
@@ -1,4 +1,4 @@
 package api
 
 //go:generate -command compile_proto protoc -I../protos
-//go:generate compile_proto todo.proto --proto_path=. --go_out=. --go-grpc_out=.
+//go:generate compile_proto todo.proto --proto_path=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative
